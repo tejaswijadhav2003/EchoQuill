d338e32366edb5ec0c4c0b72fc4e3c1fd07b7a1d
Tejaswi Jadhav <94731975+tejaswijadhav2003@users.noreply.github.com>
2024-04-02 15:32:12 +0530

    added loading

--- EchoQuill/src/Context/Context.jsx
+++ EchoQuill/src/Context/Context.jsx
@@ -1,12 +1,28 @@
-import React, { useContext, useState } from 'react'
+import { onAuthStateChanged } from 'firebase/auth';
+import React, { useContext, useEffect, useState } from 'react'
 import { createContext } from 'react'
-
+import { auth } from '../firebase/firebase'; 
+import Loading from '../components/Loading/Loading';
 const BlogContext = createContext();
 function Context({children}) {
     const [currentUser, setCurrentUser] = useState(false);
+    const [loading, setloading] = useState(true);
+    useEffect(()=>{
+      setloading(true);
+      const unsubscribe = onAuthStateChanged(auth, (user)=>{
+        if(user){
+          setCurrentUser(user);
+        }
+        else{
+          setCurrentUser(null);
+        }
+        setloading(false);
+      });
+      return ()=> unsubscribe();
+    },[currentUser]);
   return (
     <BlogContext.Provider value={{currentUser, setCurrentUser}}>
-        {children}
+        {loading? <Loading/>: children}
     </BlogContext.Provider>
   )
 }
