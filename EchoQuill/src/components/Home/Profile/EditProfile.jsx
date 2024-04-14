46a054a8248e6242b8091df1db7417711a49ce3f
Tejaswi Jadhav <94731975+tejaswijadhav2003@users.noreply.github.com>
2024-04-15 00:27:26 +0530

    debugging

---
+++ EchoQuill/src/components/Home/Profile/EditProfile.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useEffect, useRef, useState } from "react";
+import Modal from "../../../utils/Modal";
+import { LiaTimesSolid } from "react-icons/lia";
+
+function EditProfile() {
+  return (
+    <div>EditProfile</div>
+  )
+}
+
+export default EditProfile
